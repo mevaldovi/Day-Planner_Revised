4de535e94afb14b38bec4ce864aa6a9a37e35723
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-08-22 13:20:00 -0500

    minor edits

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # DayPlanner_Revised2
+
+# Description
+A highly responsive day planner to help busy individuals keep track of their daily tasks.
+
+
+# Technology
+HTML, CSS, Javascript, Bootstrap & Moment.js.
+
+
+# Table of Contents
+[Description](https://github.com/mevaldovi/DayPlanner_Revised2#Description)
+
+
+[Technology](https://github.com/mevaldovi/DayPlanner_Revised2#Technology)
+
+
+[Usage](https://github.com/mevaldovi/DayPlanner_Revised2#Usage)
+
+
+[License](https://github.com/mevaldovi/DayPlanner_Revised2#License)
+
+
+[Contact Info](https://github.com/mevaldovi/DayPlanner_Revised2#Contact)
+
+
+[Agnowledgments](https://github.com/mevaldovi/DayPlanner_Revised2#Agknowledgments)
+
+
+# Usage
+
+[Github_io](). 
+
+If the link does not work, you can just clone down this repo. Open the index.html in your code editor, right click on the index and select "Open in Default Browser".
+# License
+None at this time.
+
+# Contact
+
+Github: [https://github.com/mevaldovi](https://github.com/mevaldovi)
+
+Shoot me an email:[mevaldovi@gmail.com](mailto:mevaldovi@gmail.com)
+
+# Agknowledgments
+Special thanks to Trinh Nguyen for helping challenge me and point me in the right direction at the same time to complete this project.
+
+"Last, but not least, I would like to thank me. I want to thank me for believing in me, I want to thank me for doing all this hard work. I wanna thank me for having no days off. I wanna thank me for never quitting. I wanna thank me for always been a giver and trying to give more than I receive. I want to thank me for trying to do more right than wrong. I want to thank me for just being me at all times.” --Snoop Dog
---
+++ assets/script.js
@@ -0,0 +1,65 @@
+
+console.log("update time");
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar --CHECK!
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours --CHECK!
+// WHEN I view the timeblocks for that day
+function update() {
+    $("#currentDay").html(moment().format("MMMM D YYYY hh:mm:ss"));
+}
+
+setInterval(update, 1000);
+var timeNow = moment();
+var currentHour = timeNow.hours();
+
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+
+//var currentHour = Date.now().update();
+function timeColor() {
+    $('.time-block').each(function() {
+        var timeSection = $(this).attr("id")
+        if (timeSection < currentHour) {
+            $(this).addClass("past")
+        } else if (timeSection == currentHour) {
+            $(this).addClass("present")
+        } else {
+            $(this).removeClass("past present")
+            $(this).addClass("future")
+        }
+
+    })
+}
+
+function saveToDo()
+{
+    $('.time-block').each(function ()
+    {
+        if (localStorage.getItem(parseInt(($(this)).attr('id'))) === null)
+        {return;}
+
+        else
+        {$(this).children('.description').val(localStorage.getItem(parseInt(($(this)).attr('id'))));}
+    })
+}
+
+$('.saveBtn').click(function (event) 
+{
+    event.preventDefault();//stops Chrome from clearing entered data when refreshing the page 
+    var t = $(event.target);
+    localStorage.setItem(parseInt(t.parent().attr('id')), t.siblings('.description').val())
+})
+
+timeColor();
+saveToDo();
+
+
+// WHEN I click into a timeblock THEN I can enter an event
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+// ```
